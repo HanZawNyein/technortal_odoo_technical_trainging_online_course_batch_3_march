ef6e9a6c1ce19c34714788ed6e6611f57183dafa
HanZawNyein <hanzawnyineonline@gmail.com>
2026-03-16 20:57:11 +0630

    [ADD] ica_hms
    - rooms

--- addons/ica_hms/__manifest__.py
+++ addons/ica_hms/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    "name":"Hotel Management System",
-    "author":"Agga, IdeaCode Academy",
-    "license":"LGPL-3",
-    "data":[
+    "name": "Hotel Management System",
+    "author": "Agga, IdeaCode Academy",
+    "license": "LGPL-3",
+    "data": [
         "views/hms_room.xml",
         "views/menus.xml",
     ]
